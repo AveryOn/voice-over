70486ae70b8e83550e57fe79958dc9758db91378
AveryOn <lnkonion2@gmail.com>
2025-05-18 02:03:07 +0500

    fix

--- voiceover/speak.py
+++ voiceover/speak.py
@@ -3,9 +3,11 @@ import sys
 import sounddevice as sd
 from time import time
 import numpy as np
+import subprocess
 
 SAMPLE_RATE=16000
 
+torch.set_default_device("cpu")
 model, _ = torch.hub.load(
     repo_or_dir='snakers4/silero-models',
     model='silero_tts',
@@ -13,7 +15,25 @@ model, _ = torch.hub.load(
     speaker='baya_v2'
 )
 
+def get_power_profile():
+    try:
+        result = subprocess.run(['powerprofilesctl', 'get'], stdout=subprocess.PIPE, text=True)
+        return result.stdout.strip()
+    except Exception:
+        return None
+
 def speak(text):
+    print(f"[DEBUG] Озвучиваем текст: '{text}'")
+    if not text or not text.strip():
+        print("❌ Текст для озвучки пустой. Пропускаем.")
+        return
+
+    # profile = get_power_profile()
+    # print("[MODE]:::", profile)
+    # if profile == "performance":
+    #     SAMPLE_RATE=11025
+    #     sd.default.device = 9
+    # else:
     sd.default.device = 8
 
     start = time()
@@ -24,8 +44,10 @@ def speak(text):
     stereo_audio = np.column_stack((audio_np, audio_np))
 
     print(f"⏱️ Генерация заняла {round(time() - start, 2)} сек")
+    # sd.play(stereo_audio, SAMPLE_RATE)
     sd.play(stereo_audio, SAMPLE_RATE)
     sd.wait()
+    print(1)
 
 if __name__ == "__main__":
     speak(sys.argv[1])
